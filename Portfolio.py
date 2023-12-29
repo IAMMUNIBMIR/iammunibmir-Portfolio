3e2723fc5fd872398ad83ade17b1a0eddfa374a7
Munib <107819673+IAMMUNIBMIR@users.noreply.github.com>
2023-12-28 23:57:41 -0700

    Done

--- Portfolio.py
+++ Portfolio.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
+# Import other necessary modules if needed
+import os
 
 def concatenate_html():
     # Read the content of header.html
@@ -8,7 +7,7 @@ def concatenate_html():
         header = file.read()
 
     # Read the content of body.html
-    with open('Appearance/body.html', 'r', encoding='utf-8') as file:
+    with open('Appearance/Body.html', 'r', encoding='utf-8') as file:
         body = file.read()
 
     # Read the content of footer.html
@@ -19,15 +18,8 @@ def concatenate_html():
     combined_html = f'{header}\n{body}\n{footer}'
 
     # Write the combined content to index.html
-    with open("Appearance/index.html", "w", encoding="utf-8") as file:
+    with open("./Appearance/index.html", "w", encoding="utf-8") as file:
         file.write(combined_html)
 
 # Concatenate HTML files before running the app
 concatenate_html()
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
